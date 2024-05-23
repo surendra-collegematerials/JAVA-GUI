43415da2423aa059cb937e5635dbeb40e43912f4
surendra-collegematerials <161677934+surendra-collegematerials@users.noreply.github.com>
2024-05-23 08:02:13 +0545

    Create Pgm001_FrameExample.java

---
+++ Pgm001_FrameExample.java
@@ -0,0 +1,9 @@
+public class Pgm001_FrameExample {
+    public static void main(String[] args) {
+      JFrame frame = new JFrame();
+      frame.setTitle("HELLO WORLD");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(500,500);
+      frame.setVisible(true);
+    }
+}
